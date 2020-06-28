8b199eadd235800d91ba4c99b1b60b0604c5f5d7
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2020-06-28 20:27:58 +0200

    Add horners method

--- ComputationalMethods/com.Github.Haseoo.ComputationalMethods/Math.cs
+++ ComputationalMethods/com.Github.Haseoo.ComputationalMethods/Math.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Linq;
 
 namespace com.Github.Haseoo.ComputationalMethods
 {
     public class Math
     {
+        public static double Horners(double[] polynomialParameters,
+            double argument)
+        {
+            double value = 0;
+            polynomialParameters.Take(polynomialParameters.Length - 1)
+                .ToList()
+                .ForEach(param => value = (value + param) * argument);
+            return value + polynomialParameters.Last();
+        }
     }
 }
