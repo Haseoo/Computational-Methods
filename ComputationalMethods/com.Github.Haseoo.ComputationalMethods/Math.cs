cfd519d5f9d4e7e17878bcd66bb83c0ab4bdf00f
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2020-06-28 20:52:01 +0200

    Add calculation ofan integral using the trapezoidal method

--- ComputationalMethods/com.Github.Haseoo.ComputationalMethods/Math.cs
+++ ComputationalMethods/com.Github.Haseoo.ComputationalMethods/Math.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace com.Github.Haseoo.ComputationalMethods
 {
     public class Math
     {
-        public static double Horners(double[] polynomialParameters,
+        public static double Horners(
+            double[] polynomialParameters,
             double argument)
         {
             double value = 0;
@@ -13,6 +15,22 @@ namespace com.Github.Haseoo.ComputationalMethods
                 .ToList()
                 .ForEach(param => value = (value + param) * argument);
             return value + polynomialParameters.Last();
+        }
+        
+        public static double CalculateIntegralTrapezoidal(
+            Func<double, double> function,
+            double begin,
+            double end,
+            int accuracy)
+        {
+            Debug.Assert(begin <= end);
+            var h = (end - begin) / accuracy;
+            var returnValue = (function(begin) + function(end)) / 2.0;
+            while (begin <= end - h)
+            {
+                returnValue += function(begin += h);
+            }
+            return returnValue * h;
         }
     }
 }
